a46db036d8948f83a0ae226c7b2a550d3027aeae
JohnHazelton <john.hazelton1@gmail.com>
2021-04-24 11:00:22 -0400

    Updated stats model

--- statistical_model_class.py
+++ statistical_model_class.py
@@ -9,34 +9,33 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.metrics import r2_score, mean_squared_error
+from statsmodels.api import OLS
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-nba_data = pd.read_csv('/Users/Jakekolessar/Downloads/nba_df.csv')
-nba_data = nba_data.drop(['Rk'], axis=1)
-wnba_data = pd.read_csv('/Users/Jakekolessar/Downloads/wnba_df.csv')
-
+data = pd.read_csv('https://raw.githubusercontent.com/UVA-CS5010/semester-project/main/NBAvsWNBA.csv')
+nba_data = data[data['League']=='NBA']
+wnba_data = data[data['League']=='WNBA']
 
 class linearModel:
-    # I think most of this can be combined if there is no return
     def __init__(self, df):
         self.stats = df
-
-    def prepare_data(self):
-        # import pdb; pdb.set_trace()
+        #lean up the dataframe
+        columns_to_drop = ['League', 'Team', 'Position', 'Age', 'Avg_Salary', 'salary_ratio', 'Salary_Rank']
         self.stats = self.stats.dropna()
-        self.stats = self.stats.iloc[:, 8:]
-        self.stats = self.stats.drop(['average salary'], axis=1)
-        return self.stats
-
-    def train_test_split(self):
-        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.stats.iloc[:, :22],
-                                                                                self.stats['salary_float'],
+        self.stats = self.stats.drop(columns_to_drop, axis=1)
+        #seperate response and predictors
+        self.salary = self.stats.iloc[:, -1]
+        self.stats = self.stats.iloc[:, 4:-1]
+        # Split the sample data into train and test data
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.stats,
+                                                                                self.salary,
                                                                                 test_size=0.2, random_state=4)
-
-    def create_model(self):
+        #fit the linear model
         self.model = LinearRegression().fit(self.X_train, self.Y_train)
 
     def feature_selection(self, direction=None):
-        sfs = SequentialFeatureSelector(self.model, n_features_to_select=5, direction=direction)
+        sfs = SequentialFeatureSelector(self.model, n_features_to_select=3, direction=direction)
         self.sfs = sfs.fit(self.X_train, self.Y_train)
         self.X_train_columns = self.X_train.columns[sfs.get_support()]
         self.X_test_columns = self.X_test.columns[sfs.get_support()]
@@ -46,47 +45,49 @@ class linearModel:
 
     def predict(self):
         self.y_pred = self.model.predict(self.X_test)
-
-    def calc_r_squared(self):
+        #calculate the R squared value for the model
         self.r_squared = r2_score(self.Y_test, self.y_pred)
 
     def calc_r_squared_adjusted(self, r_squared):
         num_pred = len(self.X_train)
         pred = len(self.X_train.columns)
         r_squared = abs(r_squared)
-        import pdb;
-        pdb.set_trace()
         r_squared_adjusted = 1 - ((1 - r_squared) * (num_pred - 1) / (num_pred - pred - 1))
         return r_squared_adjusted
 
 
 model = linearModel(nba_data)
-model.prepare_data()
-model.train_test_split()
-model.create_model()
 model.predict()
-model.calc_r_squared()
 print('Full model R Squared Valued: ' + str(model.r_squared))
 
 forward_model = linearModel(nba_data)
-forward_model.prepare_data()
-forward_model.train_test_split()
-forward_model.create_model()
 forward_model.feature_selection('forward')
 forward_model.predict()
-forward_model.calc_r_squared()
 print('Forward model R Squared value: ' + str(forward_model.r_squared))
 
 backward_model = linearModel(nba_data)
-backward_model.prepare_data()
-backward_model.train_test_split()
-backward_model.create_model()
 backward_model.feature_selection('backward')
 backward_model.predict()
-backward_model.calc_r_squared()
 print('Backward model R Squared value: ' + str(backward_model.r_squared))
 
+# Pick the best model based on the R squared value
 models = {'full model': model.r_squared, 'forward_model': forward_model.r_squared,
           'backward_model': backward_model.r_squared}
 best = max(models, key=models.get)
 print('Based on the R Squared metric, the ' + str(best) + ' is the best choice.')
+
+# Print out the statistical summary for the best model
+new_model = OLS(forward_model.Y_train, forward_model.X_train).fit()
+print(new_model.summary())
+
+
+# Add data column for predicted salaries from model:
+nba_data['Predicted_Salary'] = nba_data['2P']*new_model.params[0] + nba_data['AST']*new_model.params[1] + nba_data['BLK']*new_model.params[2]
+nba_data['Predicted_Salary']
+nba_data['Salary_Residual'] = nba_data['salary_float'] - nba_data['Predicted_Salary']
+
+#NBA data sorted in order of most undervalued players:
+nba_data.sort_values('Salary_Residual', ascending=True)
+
+#NBA data sorted in order of most overvalued players:
+nba_data.sort_values('Salary_Residual', ascending=False)
